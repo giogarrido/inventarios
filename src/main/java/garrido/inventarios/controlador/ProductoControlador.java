1fafc3e44982fde9987aea47903ac65169d4f937
GARRIDO\giova <giovanni.garrido228724@potros.itson.edu.mx>
2024-03-25 23:21:29 -0700

    add actualizarProducto, eliminarProducto

--- src/main/java/garrido/inventarios/controlador/ProductoControlador.java
+++ src/main/java/garrido/inventarios/controlador/ProductoControlador.java
@@ -1,13 +1,17 @@
 package garrido.inventarios.controlador;
 
+import garrido.inventarios.excepcion.RecursoNoEncontradoExcepcion;
 import garrido.inventarios.modelo.Producto;
 import garrido.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 //http://localhost:8080/inventario-app
@@ -33,6 +37,45 @@ public class ProductoControlador {
     public Producto agregarProducto(@RequestBody Producto producto){
         LOGGER.info("Producto a agregar: "+ producto);
         return this.productoServicio.guardarProducto(producto);
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obterProductoPorId(
+            @PathVariable int id){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if(producto!=null){
+            return ResponseEntity.ok(producto);
+        }else {
+            throw new RecursoNoEncontradoExcepcion("No se encontro el ID: " + id);
+        }
+    }
+
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Producto> actualizarProucto(
+            @PathVariable int id,
+            @RequestBody Producto productoRecibido){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if (producto==null)
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        producto.setDescripcion(productoRecibido.getDescripcion());
+        producto.setPrecio(productoRecibido.getPrecio());
+        producto.setExistencia(productoRecibido.getExistencia());
+        this.productoServicio.guardarProducto(producto);
+        return ResponseEntity.ok(producto);
+
+    }
+
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Map<String, Boolean>>
+    eliminarProducto(@PathVariable int id)
+    {
+        Producto producto = productoServicio.buscarProductoPorId(id);
+        if (producto==null)
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        this.productoServicio.elminarProductoPorId(id);
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 
 
